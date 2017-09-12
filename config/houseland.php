c82767c6050911d12f5aa62a49256b035a27769f
HoangNH <hoangnh@evolableasia.vn>
2017-09-12 17:22:50 +0700

    .

--- config/houseland.php
+++ config/houseland.php
@@ -8,11 +8,11 @@ return [
     ],    
 
     'num_alert' => 10, // number rows for alert on top menu
-    'upload_path' => public_path() . '/uploads/', // media_upload_path   
-	'upload_thumbs_path' => public_path() . '/uploads/thumbs/', // media_upload_path
-    'upload_thumbs_path_articles' => public_path() . '/uploads/thumbs/articles/', // media_upload_path  
-    'upload_thumbs_path_cate' => public_path() . '/uploads/thumbs/cate/', // media_upload_path  
-    'upload_url' => config('app.url') . '/public/uploads/', // image path,
+    'upload_path' => public_path() . '/uploads/images/', // media_upload_path   
+	'upload_thumbs_path' => public_path() . '/uploads/images/thumbs/', // media_upload_path
+    'upload_thumbs_path_articles' => public_path() . '/uploads/images/thumbs/articles/', // media_upload_path  
+    'upload_thumbs_path_cate' => public_path() . '/uploads/images/thumbs/cate/', // media_upload_path  
+    'upload_url' => config('app.url') . '/public/uploads/images/', // image path,
     'max_size_upload' => 8000000    
 ];
 
