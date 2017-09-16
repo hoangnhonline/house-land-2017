02e29f761657031f085287d978b9649258f3945e
HoangNH <hoangnhonline@gmail.com>
2017-09-16 08:14:48 +0700

    .

--- resources/views/backend/services/index.blade.php
+++ resources/views/backend/services/index.blade.php
@@ -68,7 +68,7 @@
                   <a href="{{ route( 'services.edit', [ 'id' => $item->id ]) }}">{{ $item->title }}</a>
                   
                   @if( $item->is_hot == 1 )
-                  <img class="img-thumbnail" src="{{ URL::asset('public/admin/dist/img/star.png')}}" alt="Nổi bật" title="Nổi bật" />
+                  <label class="label label-danger">HOT</label>
                   @endif
 
                   <p>{{ $item->description }}</p>
