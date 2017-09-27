208df9c20b1196fc1eb474d5abbaa4f0e7dad164
HoangNH <hoangnh@evolableasia.vn>
2017-09-27 09:59:10 +0700

    .

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,10 @@ class IsAdmin {
 	public function handle($request, Closure $next)
 	{
 		if ( Auth::check() )
-        {
+        {        	
+        	if(Auth::user()->status == 2){
+        		return "Tài khoản đã bị khóa.";die;
+        	}
             return $next($request);
         }
 		return redirect()->route('backend.login-form');
