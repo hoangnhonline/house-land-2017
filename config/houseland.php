7f48191071e9300670fcc8065261c2caeebec5d4
HoangNH <hoangnhonline@gmail.com>
2017-10-03 16:34:55 +0700

    .

--- config/houseland.php
+++ config/houseland.php
@@ -10,10 +10,12 @@ return [
     'num_alert' => 10, // number rows for alert on top menu
     'upload_path' => public_path() . '/uploads/images/', // media_upload_path   
 	'upload_thumbs_path' => public_path() . '/uploads/images/thumbs/', // media_upload_path
+    'upload_thumbs_path_2' => public_path() . '/uploads/images/thumbs/175x117/', // media_upload_path
     'upload_thumbs_path_articles' => public_path() . '/uploads/images/thumbs/articles/', // media_upload_path  
     'upload_thumbs_path_cate' => public_path() . '/uploads/images/thumbs/cate/', // media_upload_path  
     'upload_url' => config('app.url') . '/public/uploads/images/', // image path,
     'upload_url_thumbs' => config('app.url') . '/public/uploads/images/thumbs/', // image path,
+    'upload_url_thumbs_2' => config('app.url') . '/public/uploads/images/thumbs/175x117/', // image path,    
     'max_size_upload' => 8000000    
 ];
 
